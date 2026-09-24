d60939c331119fa4c044434b8e3c244659bae47a
Truong Van Phong <truongvanphong12111@gmail.com>
2026-09-24 22:46:52 +0700

    add sentence playback control

--- frontend/app.js
+++ frontend/app.js
@@ -4,10 +4,24 @@ const API_URL = "http://127.0.0.1:8000";
 const startDictationButton  = document.getElementById("start-dictation-btn");
 
 
+let player;
+let currentVideoId;
+let sentenceTimer;
+const SENTENCE_PADDING = 0.2;
+
 startDictationButton.addEventListener(
     "click",
     createSession
 );
+
+function onYouTubeIframeAPIReady() {
+
+    player = new YT.Player("player", {
+        height: "360",
+        width: "640",
+    });
+
+}
 
 
 async function createSession() {
@@ -46,8 +60,39 @@ async function createSession() {
 }
 
 
+function tryLoadVideo() {
+
+    if (player && currentVideoId) {
+        player.loadVideoById(currentVideoId);
+    }
+
+}
+
+function playSentence(sentence) {
+    if (sentenceTimer){
+        clearTimeout(sentenceTimer);
+    }
+
+    const endTime = sentence.start + sentence.duration + SENTENCE_PADDING;
+
+    player.seekTo(sentence.start);
+    player.playVideo();
+
+    sentenceTimer = setInterval(() => {
+
+        if (player.getCurrentTime() >= endTime) {
+            player.pauseVideo();
+            clearInterval(sentenceTimer);
+        }
+
+    }, 100);
+}
+
 
 function renderSession(session) {
+    currentVideoId = session.video_id;
+    tryLoadVideo();
+    
 
     const info = document.getElementById("session-info");
     info.textContent =
@@ -71,7 +116,14 @@ function renderSession(session) {
              - duration: ${sentence.duration}s
              - words: ${sentence.word_count}`;
 
+        item.addEventListener("click", () => {
+            playSentence(sentence);
+        })
+
 
         list.appendChild(item);
     }
 }
+
+
+
